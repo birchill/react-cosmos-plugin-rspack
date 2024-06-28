fde038905074e41bfc080d86bb23572ae2c340ee
Brian Birtles <birtles@gmail.com>
2024-06-28 17:51:09 +0900

    chore: update dependencies

--- src/server/rspackConfig/resolveRspackLoaderPath.ts
+++ src/server/rspackConfig/resolveRspackLoaderPath.ts
@@ -4,6 +4,8 @@ import { createRequire } from 'node:module';
 // module always needs to be mocked in tests until Jest adds ESM support.
 
 export function resolveRspackLoaderPath() {
+  console.log('resolveRspackLoaderPath');
   const require = createRequire(import.meta.url);
+  console.log(require.resolve('./userImportsLoader.cjs'));
   return require.resolve('./userImportsLoader.cjs');
 }
