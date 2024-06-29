ac90cfa083773ba200bf3afcb7a9e97b37815a91
Brian Birtles <birtles@gmail.com>
2024-06-29 20:59:54 +0900

    chore: drop logging

--- src/server/rspackConfig/resolveRspackLoaderPath.ts
+++ src/server/rspackConfig/resolveRspackLoaderPath.ts
@@ -1,8 +1,6 @@
 import { createRequire } from 'node:module';
 
 export function resolveRspackLoaderPath() {
-  console.log('resolveRspackLoaderPath');
   const require = createRequire(import.meta.url);
-  console.log(require.resolve('./userImportsLoader.cjs'));
   return require.resolve('./userImportsLoader.cjs');
 }
