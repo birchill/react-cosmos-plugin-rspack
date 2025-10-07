f9f322561d449b2b467f925b9a4e083a6888e01d
Brian Birtles <birtles@gmail.com>
2025-10-07 12:56:40 +0900

    chore: switch to tsdown

---
+++ tsdown.config.ts
@@ -0,0 +1,40 @@
+import virtual from '@rollup/plugin-virtual';
+import { defineConfig } from 'tsdown';
+
+export default defineConfig([
+  // server
+  {
+    entry: {
+      'server/rspackServerPlugin': 'src/server/rspackServerPlugin.ts',
+    },
+    external: ['./userImports.js'],
+  },
+
+  // client
+  {
+    entry: {
+      'client/index': 'src/client/index.ts',
+      'client/reactDevtoolsHook': 'src/client/reactDevtoolsHook.ts',
+      'client/userImports': 'src/client/userImports.ts',
+      'client/errorOverlay/reactErrorOverlay':
+        'src/client/errorOverlay/reactErrorOverlay.ts',
+    },
+    external: ['react'],
+    platform: 'browser',
+  },
+
+  // ui
+  {
+    define: {
+      'process.env.NODE_ENV': '"production"',
+    },
+    entry: { 'ui/build': 'src/ui/WebpackRendererError.tsx' },
+    plugins: [
+      virtual({
+        react: 'module.exports = React',
+        'react-dom': 'module.exports = ReactDOM',
+        'react-plugin': 'module.exports = ReactPlugin',
+      }),
+    ],
+  },
+]);
